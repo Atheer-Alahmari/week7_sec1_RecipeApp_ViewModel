2cad2531ad2f9369bbc181f8933cd1ecd5fe327e
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-10-31 14:58:56 +0300

    Initial commit

--- app/src/main/java/com/example/week7_sec1_recipeappviewmodel/RV_Adapter.kt
+++ app/src/main/java/com/example/week7_sec1_recipeappviewmodel/RV_Adapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.week7_sec1_recipeappviewmodel.Live_Data.MyViewModel
@@ -31,8 +32,8 @@ class RV_Adapter (val activity: Update_Delete_user, private val books:List<Book>
         val userId = books[position].id
         val titel = books[position].title
         val author = books[position].author
-        val in1 =books[position].ingredients
-        val is1=books[position].instructions
+        val in1 = books[position].ingredients
+        val is1 = books[position].instructions
 
 
         holder.itemView.apply {
@@ -41,14 +42,14 @@ class RV_Adapter (val activity: Update_Delete_user, private val books:List<Book>
             text_View3.text = in1
             text_View4.text = is1
 
-         //   var myDBRoom= BookDataBase.getInstance(activity)
-             val myViewModel by lazy { ViewModelProvider(activity).get(MyViewModel::class.java)}//------------------------
+            //   var myDBRoom= BookDataBase.getInstance(activity)
+            val myViewModel by lazy { ViewModelProvider(activity).get(MyViewModel::class.java) }//------------------------
 
             //-----------------------------AlertDialog---------------------------------------------------
             editIcon.setOnClickListener {
                 var alt = AlertDialog.Builder(activity)
                 alt.setTitle("Update Book ")
-                val mLayout  = LinearLayout(activity)
+                val mLayout = LinearLayout(activity)
                 val mEtTitel = EditText(activity)
                 val mEtAt = EditText(activity)
                 val mEtIng = EditText(activity)
@@ -72,19 +73,24 @@ class RV_Adapter (val activity: Update_Delete_user, private val books:List<Book>
                 mEtInst.setText(is1)
 
 
-
                 // --------------------Positive button text and action
 
-                alt.setPositiveButton("Save", DialogInterface.OnClickListener { _, _ ->
+                alt.setPositiveButton("Update", DialogInterface.OnClickListener { _, _ ->
                     //mEtName.setText(celeName)
                     var inputName = mEtTitel.text.toString()
                     var inputT1 = mEtAt.text.toString()
                     var inputT2 = mEtIng.text.toString()
                     var inputT3 = mEtInst.text.toString()
 
-                   // myDBRoom.BookDao1().updateBook(Book(userId,inputName,inputT1,inputT2,inputT3))
-myViewModel.update_Book(userId,inputName,inputT1,inputT2,inputT3)
-                    activity.updateList()//text_View.text =input
+                    // myDBRoom.BookDao1().updateBook(Book(userId,inputName,inputT1,inputT2,inputT3))
+                    if (inputName.isNotEmpty() && inputT1.isNotEmpty() && inputT2.isNotEmpty() && inputT3.isNotEmpty()) {
+                        myViewModel.update_Book(userId, inputName, inputT1, inputT2, inputT3)
+                        activity.updateList()//text_View.text =input
+
+                        Toast.makeText(activity, "Update Success!", Toast.LENGTH_SHORT).show()
+                    }else {
+                        Toast.makeText(activity, "Update Doesn't Work , Something Empty ", Toast.LENGTH_SHORT).show()
+                    }
 
                 })
 
@@ -102,8 +108,10 @@ myViewModel.update_Book(userId,inputName,inputT1,inputT2,inputT3)
             }
             //------------------------------------------------------------------------------------
             deleteIcon.setOnClickListener {
-               // myDBRoom.BookDao1().deleteBook(Book(userId))
+                // myDBRoom.BookDao1().deleteBook(Book(userId))
                 myViewModel.delete_Book(userId)
+                Toast.makeText(activity, "Delete Success!", Toast.LENGTH_SHORT).show()
+
                 activity.updateList()
             }
 
